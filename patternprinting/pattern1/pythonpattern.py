aab6c3b995910d107022e57842808fc991063040
Nova <beautymaurya9142@gmail.com>
2025-11-11 15:21:48 +0530

    Add files via upload

---
+++ patternprinting/pattern1/pythonpattern.py
@@ -0,0 +1,15 @@
+# n=int(input("enter the row: "))
+# # col=int(input("enter the col"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#       print(j,end=" ")
+#     print()
+   
+
+
+maths=int(input("enter the maths marks: "))
+eng=int(input("enter the eng marsk: "))
+hin=int(input("enter the hindi marks:"))
+sci=int(input("enter the sci marsk: "))
+geo=int(input("enter the geo marks:"))
+total=0
